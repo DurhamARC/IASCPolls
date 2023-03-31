5a5f7f24da569190bad292325e88915f8424fe0c
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-03-31 17:02:08 +0100

    Set STATICFILES_DIRS

--- iasc/settings.py
+++ iasc/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "frontend/static"),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
