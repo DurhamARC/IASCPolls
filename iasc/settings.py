17ecd3a5247b9945cbf63c3e723fffcb134736be
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-04-14 11:58:23 +0100

    Switch to MySQL database from sqlite

--- iasc/settings.py
+++ iasc/settings.py
@@ -23,7 +23,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -92,17 +91,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "iasc.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env.str("DB_NAME", "arc_iasc"),
+        "USER": env.str("DB_USER", "arc_iasc"),
+        "PASSWORD": env.str("DB_PASS", "1234"),
+        "HOST": env.str("DB_HOST", "127.0.0.1"),
+        "PORT": env.str("DB_PORT", "3306"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -122,7 +123,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -130,7 +130,6 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
