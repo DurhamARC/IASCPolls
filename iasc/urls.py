4445d90789a821962f707fa51c783c4c1e6c3bd8
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-05-15 17:57:12 +0100

    Remove redundant filtering from SurveyViewSet

--- iasc/urls.py
+++ iasc/urls.py
@@ -22,7 +22,6 @@ from iasc import views
 router = routers.DefaultRouter()
 api_views = [
     (r"participants", views.ParticipantViewSet, "participants"),
-    (r"survey/(?P<survey_id>.+)/", views.SurveyViewSet, "survey"),
     (r"survey", views.SurveyViewSet, "survey"),
     (r"links", views.ActiveLinkViewSet, "links"),
     (r"result", views.ResultViewSet, "result"),
