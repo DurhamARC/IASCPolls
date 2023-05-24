75509324ed7e336a304587b415bcab7a2d396525
joannercsheppard <joannesheppard1997@outlook.com>
2023-05-24 16:49:52 +0100

    home changes

--- react-app/src/pages/Thanks.js
+++ react-app/src/pages/Thanks.js
@@ -4,20 +4,22 @@ import Footer from '../components/Footer';
 
 export default function Thanks() {
   return (
-    <div className="thankyou--container">
+    <div>
       <NavBar />
+      <div className="thankyou--container">
       <div className="container centered">
-      <div className="thankyou">
-        <h1>Thank you for your participation!</h1>
-        <div>
-          <p>Thank you for participating in our beta test. Your input and feedback have been invaluable 
-            in helping us shape and improve our product. We greatly appreciate your time and effort. If you'd like to learn more 
-            about the research and development behind our product, <a href="/ethics"> please follow this link. </a> </p>
-          <p>Once again, thank you for your support.</p>
-        </div>
+        <div className="thankyou">
+          <h1>Thank you for your participation!</h1>
+          <div>
+            <p>Thank you for participating in our beta test. Your input and feedback have been invaluable 
+              in helping us shape and improve our product. We greatly appreciate your time and effort. If you'd like to learn more 
+              about the research and development behind our product, <a href="/ethics"> please follow this link. </a> </p>
+            <p>Once again, thank you for your support.</p>
+          </div>
         </div>
       </div>
       <Footer />
+      </div>
     </div>
-  )
-};
+  );
+}
