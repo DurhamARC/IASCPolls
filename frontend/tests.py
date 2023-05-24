9c5c60f42d02c2b6dfee3dc12df53bf9ea3c04cb
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-05-24 13:26:24 +0100

    📝 Testcases for frontend module

--- frontend/tests.py
+++ frontend/tests.py
@@ -1,3 +1,108 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from frontend import urls
+
+
+class HTTPTestCase(TestCase):
+    """
+    Superclass for HTTP Test cases, providing some nice helper functions
+    """
+
+    def setUp(self):
+        # Client instance to test routes
+        self.client = Client()
+
+    def GET(
+        self,
+        url,
+        status=200,
+        mimetype="text/html",
+        startswith=b"<!DOCTYPE html>",
+        contains=None,
+    ):
+        """
+        Make GET request and test response for status, mimetype, start, and content
+        """
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, status)
+        self.failUnless(response.headers["Content-Type"].startswith(mimetype))
+
+        if startswith:
+            self.assertTrue(response.content.startswith(startswith))
+        if contains:
+            self.assertContains(response, contains)
+
+    def POST(self, url, params, status=200, mimetype="text/html", contains=None):
+        """
+        Make POST request and test response
+        """
+        response = self.client.post(url, params)
+        self.assertEquals(response.status_code, status)
+        self.failUnless(response.headers["Content-Type"].startswith(mimetype))
+        if contains:
+            self.assertContains(response, contains)
+        return response
+
+
+class FrontendViewsTestCase(HTTPTestCase):
+    def test_index(self):
+        self.GET(
+            "/",
+            contains="<title>Institute for Ascertaining Scientific Consensus</title>",
+        )
+
+
+class TestViewsTestCase(HTTPTestCase):
+    urls = "frontend.urls"
+
+    def setUp(self):
+        super(TestViewsTestCase, self).setUp()
+        self.original_urls = urls.urlpatterns
+        urls.urlpatterns += urls.testpatterns
+
+    def test_views_test(self):
+        self.GET(
+            "/test/",
+            contains="<h1>Test Pages</h1>",
+        )
+
+    def test_views_upload(self):
+        self.GET(
+            "/test/upload/",
+            contains="<h1>Upload Participants</h1>",
+        )
+
+    def test_views_survey(self):
+        self.GET(
+            "/test/survey/",
+            contains="<h1>Create Survey</h1>",
+        )
+
+    def test_views_links(self):
+        self.GET(
+            "/test/links/",
+            contains="<h1>Download Links</h1>",
+        )
+
+    def test_views_vote(self):
+        self.GET(
+            "/test/vote/",
+            contains="<h1>Vote</h1>",
+        )
+
+    def test_views_close(self):
+        self.GET(
+            "/test/close/",
+            contains="<h1>Close Survey</h1>",
+        )
+
+    def test_views_results(self):
+        self.GET(
+            "/test/results/",
+            contains="<h1>Download Results</h1>",
+        )
+
+    def tearDown(self):
+        super(TestViewsTestCase, self).tearDown()
+        urls.urlpatterns = self.original_urls
--- frontend/urls.py
+++ frontend/urls.py
@@ -7,20 +7,21 @@ urlpatterns = [
     path("", views.index),
     path("about", views.index),
     path("ethics", views.index),
-    path("poll-temp", views.index),
     path("poll", views.index),
     path("dashboard", views.index),
     path("create", views.index),
     path("favicon.ico", RedirectView.as_view(url=settings.STATIC_URL + "favicon.ico")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += [
-        path("test/", views.TestViews.index),
-        path("test/upload/", views.TestViews.upload),
-        path("test/survey/", views.TestViews.survey),
-        path("test/links/", views.TestViews.download),
-        path("test/vote/", views.TestViews.vote),
-        path("test/close/", views.TestViews.close),
-        path("test/results/", views.TestViews.results),
-    ]
+testpatterns = [
+    path("test/", views.TestViews.index),
+    path("test/upload/", views.TestViews.upload),
+    path("test/survey/", views.TestViews.survey),
+    path("test/links/", views.TestViews.download),
+    path("test/vote/", views.TestViews.vote),
+    path("test/close/", views.TestViews.close),
+    path("test/results/", views.TestViews.results),
+]
+
+if settings.ENABLE_TEST_VIEWS:
+    urlpatterns += testpatterns
