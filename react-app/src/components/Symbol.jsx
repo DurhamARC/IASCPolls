d863408495a3d4657369265ed3b63789842bef57
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-05-25 14:50:54 +0100

    🧚‍ Lint Symbol.jsx

--- react-app/src/components/Symbol.jsx
+++ react-app/src/components/Symbol.jsx
@@ -10,16 +10,22 @@ export default function Symbol({
   const setActive = () => {
     // post this to a database
     console.log(surveyId);
-    axios.post("/api/survey/close/", {
-      survey: surveyId
-    }).then(() => {
-      alert("Survey closed, all active links deleted.")
-    });
+    axios
+      .post("/api/survey/close/", {
+        survey: surveyId,
+      })
+      .then(() => {
+        alert("Survey closed, all active links deleted.");
+      });
   };
 
   const handleClick = () => {
     if (isActive) {
-      if (window.confirm("Are you sure you want to close the survey? All links will be deleted.")) {
+      if (
+        window.confirm(
+          "Are you sure you want to close the survey? All links will be deleted."
+        )
+      ) {
         setActive(!isActive);
       }
     }
@@ -27,7 +33,7 @@ export default function Symbol({
 
   const style = {
     color: isActive ? "blue" : "red",
-    cursor: isActive ? "pointer" : "default"
+    cursor: isActive ? "pointer" : "default",
   };
 
   return (
