4e199c85db6317d67afb173617484bd465aa78e1
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-05-25 14:49:07 +0100

    🐏 `eslint --fix .` Jo's Home changes

--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
@@ -9,22 +9,27 @@ function Home() {
       <div className="container home--container">
         <div className="home">
           <div>
-            <h1 className="home--title">Institute for Ascertaining Scientific Consensus</h1>
+            <h1 className="home--title">
+              Institute for Ascertaining Scientific Consensus
+            </h1>
           </div>
         </div>
         <div className="home">
-          <div className="home-pic"/>
+          <div className="home-pic" />
         </div>
       </div>
 
       <div className="container home--eyecatching">
         <div className="home">
-          <div className="home-pic-2"/>
+          <div className="home-pic-2" />
         </div>
         <div className="home">
           <div className="home--eyecatching--content">
             <h1>Ethics and Anonymisation</h1>
-            <h2>We are committed to guaranteeing the anonymity of all survey participants to ensure their safety in expressing their opinions.</h2>
+            <h2>
+              We are committed to guaranteeing the anonymity of all survey
+              participants to ensure their safety in expressing their opinions.
+            </h2>
             <a href="./ethics">
               <button type="button" className="button home--button">
                 Explore the ethics
@@ -40,16 +45,17 @@ function Home() {
             <div className="home--about--text">
               <h2>What is IASC?</h2>
               <h3>
-              The Institute for Ascertaining Scientific Consensus (IASC) is a global
-              network that can email over 100,000 scientists with a specific
-              statement of interest, such as &ldquo;Covid is caused by a
-              virus&rdquo;, and request an agree/disagree response within two
-              minutes. The responses will be instantly and anonymously recorded in a
-              database, allowing for the calculation of the strength of consensus.
-              This network will be a valuable resource for policymakers and the
-              general public in discerning the level of agreement among scientists
-              on various issues and combating misinformation. It will also provide
-              insight into cross-cultural differences in scientific opinions.
+                The Institute for Ascertaining Scientific Consensus (IASC) is a
+                global network that can email over 100,000 scientists with a
+                specific statement of interest, such as &ldquo;Covid is caused
+                by a virus&rdquo;, and request an agree/disagree response within
+                two minutes. The responses will be instantly and anonymously
+                recorded in a database, allowing for the calculation of the
+                strength of consensus. This network will be a valuable resource
+                for policymakers and the general public in discerning the level
+                of agreement among scientists on various issues and combating
+                misinformation. It will also provide insight into cross-cultural
+                differences in scientific opinions.
               </h3>
               <div>
                 <a href="./about">
