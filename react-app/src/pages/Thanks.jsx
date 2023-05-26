9e338a85783867c7c54790c96f294c1a4757f78b
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-05-26 17:07:46 +0100

    🏃‍ Alter participant-facing pages following meeting with Peter

--- react-app/src/pages/Thanks.jsx
+++ react-app/src/pages/Thanks.jsx
@@ -16,9 +16,18 @@ export default function Thanks() {
               Institute for Ascertaining Scientific Consensus. We greatly
               appreciate your time and effort. If you&apos;d like to learn more
               about the research and development behind IASC,
-              <a href="/ethics"> please follow this link. </a>
+              <a href="https://www.durham.ac.uk/research/institutes-and-centres/humanities-engaging-science-society/the-institute-for-ascertaining-scientific-consensus/">
+                {" "}
+                please visit the Institute webpage.{" "}
+              </a>
             </p>
             <p>Once again, thank you for your support.</p>
+            <p>
+              For enquiries, comments and feedback, please contact{" "}
+              <a href="mailto:Institute for Ascertaining Scientific Consensus <iasc@durham.ac.uk>">
+                The Institute for Ascertaining Scientific Consensus
+              </a>
+            </p>
           </div>
         </div>
       </div>
