75b3270e8607e0a039bfe2be6acd48fe40db66a3
joannercsheppard <joannesheppard1997@outlook.com>
2023-06-01 11:00:08 +0100

    wasting more time on the front-page...

--- react-app/src/pages/home/Home.jsx
+++ react-app/src/pages/home/Home.jsx
@@ -2,46 +2,42 @@ import React from "react";
 import NavBar from "../../components/nav/NavBar";
 import Footer from "../../components/footer/Footer";
 import "./home.css";
+import mobileFrontPage from "../../imgs/mobile-front-page.png";
+import frontPageComp from "../../imgs/balance.png";
+import frontPageScientists from "../../imgs/scientists-shaking-hands.png";
+import magnifyingGlassScientistsBackground from "../../imgs/magnifying-glass-scientists-background.png";
 
 function Home() {
+  const isMobile = window.innerWidth <= 768; // Check if the window width is less than or equal to 768px
+
   return (
     <div className="home-all">
       <NavBar />
       <div className="container home--container">
-        <div className="home">
+        <div>
           <div>
             <h1 className="home--title">
               Institute for Ascertaining Scientific Consensus
             </h1>
           </div>
         </div>
-        <div className="home">
-          <div className="home-pic" />
-        </div>
-      </div>
-
-      <div className="container home--ethics--container">
-        <div className="home">
-          <div className="home-pic-2" />
-        </div>
-        <div className="home">
-          <div className="home--ethics--content">
-            <h1>Ethics and Anonymisation</h1>
-            <h2>
-              We are committed to guaranteeing the anonymity of all survey
-              participants to ensure their safety in expressing their opinions.
-            </h2>
-            <a href="./ethics">
-              <button type="button" className="button home--button">
-                Explore the ethics
-              </button>
-            </a>
-          </div>
+        <div>
+          <div
+            className={isMobile ? "home-pic-mobile" : "home-pic"}
+            style={{
+              backgroundImage: `url(${
+                isMobile ? mobileFrontPage : magnifyingGlassScientistsBackground
+              })`,
+            }}
+          />
         </div>
       </div>
 
       <div className="container home--about--container">
-        <div className="home-pic-3">
+        <div
+          className="home-pic-3"
+          style={{ backgroundImage: `url(${frontPageScientists})` }}
+        >
           <div className="home--about--overlay">
             <div className="home--about--text">
               <h2>What is IASC?</h2>
@@ -61,10 +57,35 @@ function Home() {
               <div>
                 <a href="./about">
                   <button type="button" className="button home--button">
-                    Find out more
+                    Find Out More
                   </button>
                 </a>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container home--ethics--container">
+        <div>
+          <div
+            className="home-pic-2"
+            style={{ backgroundImage: `url(${frontPageComp})` }}
+          />
+        </div>
+        <div>
+          <div className="home--ethics--content">
+            <h1>Ethics and Anonymisation</h1>
+            <h2>
+              We are committed to guaranteeing the anonymity of all survey
+              participants to ensure their safety in expressing their opinions.
+            </h2>
+            <div>
+              <a href="./ethics">
+                <button type="button" className="button home--button">
+                  Explore
+                </button>
+              </a>
             </div>
           </div>
         </div>
