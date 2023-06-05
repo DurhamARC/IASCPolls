c75be3e0aebac094405f5437d6fbb55165a5326a
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-06-05 18:04:02 +0100

    👗 Use react-router Links in NavBar and update CSS

--- react-app/src/components/nav/NavBar.jsx
+++ react-app/src/components/nav/NavBar.jsx
@@ -1,22 +1,38 @@
 import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import { AuthConsumer } from "../AuthContext";
 import "./nav.css";
 
-export default function NavBar() {
+export default function NavBar({ className }) {
+  const location = useLocation().pathname.substring(1);
+
+  const cssClass = (() => {
+    let cls = "";
+    if (className) cls += `nav-${className} `;
+
+    if (location === "") {
+      cls += "nav-index";
+    } else {
+      cls += `nav-${location}`;
+    }
+
+    return cls;
+  })();
+
   return (
     <AuthConsumer>
       {({ isAuth }) => (
-        <nav className="nav">
+        <nav className={`nav ${cssClass}`}>
           <div className="site-title">
-            <a href="/">
-              <div className="nav-logo" />
-            </a>
+            <NavLink to="/" className="nav-logo" />
           </div>
           <li>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/ethics">Ethics</a>
-            {isAuth ? <a href="/logout">Logout</a> : <a href="/login">Login</a>}
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/ethics">Ethics</NavLink>
+            <NavLink to={isAuth ? "/logout" : "/login"}>
+              {isAuth ? "Logout" : "Login"}
+            </NavLink>
           </li>
         </nav>
       )}
