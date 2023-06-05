35146a754539d3c8c35403eb5794803f86440b18
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-06-05 19:07:32 +0100

    🔐 Cache user authentication status in localStorage

--- react-app/src/components/AuthContext.jsx
+++ react-app/src/components/AuthContext.jsx
@@ -2,10 +2,43 @@ import React, { useMemo, useState } from "react";
 
 export const AuthContext = React.createContext(null);
 
+/**
+ * Context used for user authentication. Uses localStorage to persist
+ * login status across page refreshes.
+ *
+ * @param children
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [isAuth, setAuth] = useState(null);
+  const [currentUser, _setCurrentUser] = useState(
+    localStorage.getItem("currentUser")
+  );
+  const [isAuth, _setAuth] = useState(localStorage.getItem("isAuth"));
 
+  /**
+   * Override setAuth function from useState to persist value in localStorage
+   * @param bool
+   */
+  const setAuth = (bool) => {
+    _setAuth(bool);
+    localStorage.setItem("isAuth", bool);
+  };
+
+  /**
+   * Override setCurrentUser function from useState to persist value in localStorage
+   * @param username
+   */
+  const setCurrentUser = (username) => {
+    _setCurrentUser(username);
+    localStorage.setItem("currentUser", username);
+  };
+
+  /**
+   * Use React Memo to cache the context; otherwise component re-render will cause issues
+   * Returns the stored value from the last render (unless changed)
+   * @type {{setCurrentUser: function(*): void, isAuth: string, setAuth: function(*): void, currentUser: string}}
+   */
   const contextValue = useMemo(
     () => ({
       isAuth,
@@ -16,6 +49,9 @@ function AuthProvider({ children }) {
     [isAuth, setAuth, currentUser, setCurrentUser]
   );
 
+  /**
+   * Return JSX component
+   */
   return (
     <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
   );
