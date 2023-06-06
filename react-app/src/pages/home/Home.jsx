87e97d8334098cff02d6e9e963f9756c949661e5
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-06-06 19:17:06 +0100

    🧰 Fix homepage mobile CSS

--- react-app/src/pages/home/Home.jsx
+++ react-app/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import "./home.css";
 import mobileFrontPage from "../../imgs/min/mobile-front-page.webp";
 import frontPageComp from "../../imgs/min/balance.svg";
@@ -6,27 +6,38 @@ import frontPageScientists from "../../imgs/min/scientists-shaking-hands.webp";
 import magnifyingGlassScientistsBackground from "../../imgs/min/magnifying-glass-scientists-background.webp";
 
 function Home() {
-  const isMobile = window.innerWidth <= 768; // Check if the window width is less than or equal to 768px
+  // Check if the window width is less than or equal to 768px
+  const [isMobile, _setIsMobile] = useState(window.innerWidth <= 768);
 
+  /**
+   * Update isMobile when window is resized by hand
+   */
+  useLayoutEffect(() => {
+    const setIsMobile = () => {
+      console.log(window.innerWidth <= 768);
+      _setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", setIsMobile);
+  });
+
+  /**
+   * Render function JSX
+   */
   return (
     <div className="home-all">
       <div className="container home--container">
+        <div
+          className={isMobile ? "home-pic-mobile" : "home-pic"}
+          style={{
+            backgroundImage: `url(${
+              isMobile ? mobileFrontPage : magnifyingGlassScientistsBackground
+            })`,
+          }}
+        />
         <div>
-          <div>
-            <h1 className="home--title">
-              Institute for Ascertaining Scientific Consensus
-            </h1>
-          </div>
-        </div>
-        <div>
-          <div
-            className={isMobile ? "home-pic-mobile" : "home-pic"}
-            style={{
-              backgroundImage: `url(${
-                isMobile ? mobileFrontPage : magnifyingGlassScientistsBackground
-              })`,
-            }}
-          />
+          <h1 className="home--title">
+            Institute for Ascertaining Scientific Consensus
+          </h1>
         </div>
       </div>
 
@@ -34,57 +45,48 @@ function Home() {
         <div
           className="home-pic-3"
           style={{ backgroundImage: `url(${frontPageScientists})` }}
-        >
-          <div className="home--about--overlay">
-            <div className="home--about--text">
-              <h2>What is IASC?</h2>
-              <h3>
-                The Institute for Ascertaining Scientific Consensus (IASC) is a
-                global network that can email over 100,000 scientists with a
-                specific statement of interest, such as &ldquo;Covid is caused
-                by a virus&rdquo;, and request an agree/disagree response within
-                two minutes. The responses will be instantly and anonymously
-                recorded in a database, allowing for the calculation of the
-                strength of consensus. This network will be a valuable resource
-                for policymakers and the general public in discerning the level
-                of agreement among scientists on various issues and combating
-                misinformation. It will also provide insight into cross-cultural
-                differences in scientific opinions.
-              </h3>
-              <div>
-                <a href="./about">
-                  <button type="button" className="button home--button">
-                    Find Out More
-                  </button>
-                </a>
-              </div>
-            </div>
+        />
+        <div className="home--about--overlay">
+          <div className="home--about--text">
+            <h2>What is IASC?</h2>
+            <p>
+              The Institute for Ascertaining Scientific Consensus (IASC) is a
+              global network that can email over 100,000 scientists with a
+              specific statement of interest, such as &ldquo;Covid is caused by
+              a virus&rdquo;, and request an agree/disagree response within two
+              minutes. The responses will be instantly and anonymously recorded
+              in a database, allowing for the calculation of the strength of
+              consensus. This network will be a valuable resource for
+              policymakers and the general public in discerning the level of
+              agreement among scientists on various issues and combating
+              misinformation. It will also provide insight into cross-cultural
+              differences in scientific opinions.
+            </p>
+            <a href="./about">
+              <button type="button" className="button home--button">
+                Find Out More
+              </button>
+            </a>
           </div>
         </div>
       </div>
 
       <div className="container home--ethics--container">
-        <div>
-          <div
-            className="home-pic-2"
-            style={{ backgroundImage: `url(${frontPageComp})` }}
-          />
-        </div>
-        <div>
-          <div className="home--ethics--content">
-            <h1>Ethics and Anonymisation</h1>
-            <h2>
-              We are committed to guaranteeing the anonymity of all survey
-              participants to ensure their safety in expressing their opinions.
-            </h2>
-            <div>
-              <a href="./ethics">
-                <button type="button" className="button home--button">
-                  Explore
-                </button>
-              </a>
-            </div>
-          </div>
+        <div
+          className="home-pic-2"
+          style={{ backgroundImage: `url(${frontPageComp})` }}
+        />
+        <div className="home--ethics--content">
+          <h2>Ethics and Anonymisation</h2>
+          <p>
+            We are committed to guaranteeing the anonymity of all survey
+            participants to ensure their safety in expressing their opinions.
+          </p>
+          <a href="./ethics">
+            <button type="button" className="button home--button">
+              Explore
+            </button>
+          </a>
         </div>
       </div>
     </div>
