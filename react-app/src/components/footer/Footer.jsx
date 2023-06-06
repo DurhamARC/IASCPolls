5623f359743f7ca438754b6b8551d174d5910df6
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-06-06 16:15:10 +0100

    🧭 Move Login/out link into Footer per Alpha test feedback, replace with Admin links when logged in.

--- react-app/src/components/footer/Footer.jsx
+++ react-app/src/components/footer/Footer.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { AuthConsumer } from "../AuthContext";
 import "./footer.css";
 
+/**
+ * Render the site Footer.
+ *
+ * Uses AuthConsumer to provide isAuth status, to determine whether
+ * to show the "Login" or "Logout" link. Direct child of Consumer
+ * must be a function, as below: it cannot contain JSX elements.
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function Footer() {
   return (
-    <nav className="footer">
-      <p>Institute for Ascertaining Scientific Consensus</p>
-      <li>
-        <a href="/ethics">Ethics</a>
-        <a href="/about">About</a>
-      </li>
-    </nav>
+    <AuthConsumer>
+      {({ isAuth }) => (
+        <nav className="footer">
+          <div>
+            <NavLink to="/ethics">Ethics</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to={isAuth ? "/logout" : "/login"}>
+              {isAuth ? "Logout" : "Login"}
+            </NavLink>
+          </div>
+        </nav>
+      )}
+    </AuthConsumer>
   );
 }
--- react-app/src/components/nav/NavBar.jsx
+++ react-app/src/components/nav/NavBar.jsx
@@ -3,6 +3,16 @@ import { NavLink, useLocation } from "react-router-dom";
 import { AuthConsumer } from "../AuthContext";
 import "./nav.css";
 
+/**
+ * Render the site NavBar
+ *
+ * Uses NavLinks from React-Router to allow styling links
+ * based on their 'active' class. This works because NavBar
+ * is a child of the <BrowserRouter> element in App.jsx
+ * @param className
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function NavBar({ className }) {
   const location = useLocation().pathname.substring(1);
 
@@ -26,14 +36,17 @@ export default function NavBar({ className }) {
           <div className="site-title">
             <NavLink to="/" className="nav-logo" />
           </div>
-          <li>
+          <div>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/ethics">Ethics</NavLink>
-            <NavLink to={isAuth ? "/logout" : "/login"}>
-              {isAuth ? "Logout" : "Login"}
-            </NavLink>
-          </li>
+            {isAuth && (
+              <>
+                <NavLink to="/dashboard">Dashboard</NavLink>
+                <NavLink to="/logout">Logout</NavLink>
+              </>
+            )}
+          </div>
         </nav>
       )}
     </AuthConsumer>
