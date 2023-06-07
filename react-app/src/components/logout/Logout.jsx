350b63c227301a2fc8c9521bcd46d18d5c10b27d
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2023-06-07 10:12:12 +0100

    🔧 Fix logout route

--- react-app/src/components/logout/Logout.jsx
+++ react-app/src/components/logout/Logout.jsx
@@ -1,16 +1,15 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
 import "./logout.css";
 
 export default function Logout({ to, children }) {
-  const navigate = useNavigate();
   const { setAuth, setCurrentUser } = useContext(AuthContext);
 
   const logout = () => {
     setAuth(false);
     setCurrentUser(null);
-    navigate(to);
+    // Navigate to Django logout route
+    window.location.href = to;
   };
 
   return (
